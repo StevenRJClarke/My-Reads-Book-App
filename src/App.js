35128da3fd1a0d439725359340f7e61e3045f8b5
StevenRJClarke1985 <srjc3@cantab.net>
2019-01-05 18:52:10 +0000

    Import BooksAPI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
