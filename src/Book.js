c17b2f5e1035fc60082505230e33389352159909
StevenRJClarke1985 <srjc3@cantab.net>
2019-01-06 19:13:12 +0000

    Reformatted <ShelfChanger/> so each prop on a separate line

--- src/Book.js
+++ src/Book.js
@@ -18,7 +18,9 @@ class Book extends React.Component {
         <div className="book">
           <div className="book-top">
             <img className="book-cover" style={{ width: 128, height: 193}} src={this.props.book.imageLinks.thumbnail} alt={this.props.book.title}/>
-            <ShelfChanger changeShelf={this.changeShelf}/>
+            <ShelfChanger
+              changeShelf={this.changeShelf}
+            />
           </div>
           <div className="book-title">{this.props.book.title}</div>
           <div className="book-authors">{
