60e58576e81b321c376af04e3ef07a4d8bf1422f
StevenRJClarke1985 <srjc3@cantab.net>
2019-01-06 18:21:07 +0000

    Added shelf state to Book Component

--- src/Book.js
+++ src/Book.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ShelfChanger from './ShelfChanger'
 
 class Book extends React.Component {
+  state = {
+    shelf: ''
+  }
+
   render() {
     return (
       <li>
