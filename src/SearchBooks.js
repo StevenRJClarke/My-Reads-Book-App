e147ddcf42a818d7f02f5e28ce7c8f30ee916bcb
StevenRJClarke1985 <srjc3@cantab.net>
2019-01-12 14:23:03 +0000

    Added searchedBooks state

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom'
 
 class SearchBooks extends React.Component {
   state = {
-    query: ''
+    query: '',
+    searchedBooks: []
   }
 
   updateQuery = (newQuery) => {
