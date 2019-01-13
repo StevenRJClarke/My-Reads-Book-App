5fd9174c0c2487840a0b00b1acced5de534ec389
StevenRJClarke1985 <srjc3@cantab.net>
2019-01-13 13:12:26 +0000

    Conditional checks imageLinks and imageLinks.thumbnail exist for <Book/>

--- src/Book.js
+++ src/Book.js
@@ -30,7 +30,7 @@ class Book extends React.Component {
               className="book-cover"
               style={{ width: 128, height: 193}}
               src={
-                this.props.book.imageLinks.thumbnail
+                (this.props.book.imageLinks && this.props.book.imageLinks.thumbnail)
                 ? this.props.book.imageLinks.thumbnail
                 : 'http://via.placeholder.com/128x193?text=?'
               }
