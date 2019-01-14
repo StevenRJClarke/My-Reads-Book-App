729ca0bcbc209c7489bef5c248982f6984fe13fc
StevenRJClarke1985 <srjc3@cantab.net>
2019-01-14 07:09:34 +0000

    Pasted hardcoded HTML into BookShelf Component --TO BE AMENDED

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,7 +1,32 @@
 import React from 'react'
 
 class BookShelf extends React.Component {
-
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {/* If shelf is Currently Reading, render the book here
+                using <Book/> component
+              */}
+            {
+              this.state.books
+              .filter( (book) => book.shelf === 'currentlyReading' )
+              .map( (book) => (
+                <Book
+                  book={book}
+                  key={book.id}
+                  addBookToShelves={this.addBookToShelves}
+                  changeBookShelf={this.changeBookShelf}
+                />
+              ))
+            }
+          </ol>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default BookShelf 
+export default BookShelf
